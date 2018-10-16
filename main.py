668fcea645b354dd4628ff024338077335f959ea
mfarina1 <m.farina@ufl.edu>
2018-10-16 15:48:15 -0400

    second commit

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-def key_mod_hash():
+def key_mod_hash(key, length_of_hash_table):
     x = 5
     print(x)
 
 
-def mid_digits_sqr_hash():
+def mid_digits_sqr_hash(key, length_of_hash_table):
     y = "bob"
     print(y)
 
