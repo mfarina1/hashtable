883f70b06d5e2fb4d9030e8e2237f8cb3938a305
mfarina1 <m.farina@ufl.edu>
2018-10-16 15:47:38 -0400

    second commit

---
+++ main.py
@@ -0,0 +1,13 @@
+def key_mod_hash():
+    x = 5
+    print(x)
+
+
+def mid_digits_sqr_hash():
+    y = "bob"
+    print(y)
+
+
+if __name__ == '__main__':
+    key_mod_hash()
+    mid_digits_sqr_hash()
